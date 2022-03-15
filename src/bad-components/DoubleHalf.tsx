eb5074d938cf64905fa4071d4a1b0904967e53e7
dhp2616 <dhp2616@gmail.com>
2022-03-15 14:25:22 -0400

    finished task 9

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,24 +1,28 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
+interface share {
+    setDhValue: (x: number) => void;
+    dhValue: number;
+}
 
-function Doubler(): JSX.Element {
+function Doubler({ setDhValue, dhValue }: share): JSX.Element {
     return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
 }
 
-function Halver(): JSX.Element {
+function Halver({ setDhValue, dhValue }: share): JSX.Element {
     return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
 }
 
 export function DoubleHalf(): JSX.Element {
+    const [dhValue, setDhValue] = useState<number>(10);
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Doubler dhValue={dhValue} setDhValue={setDhValue} />
+            <Halver dhValue={dhValue} setDhValue={setDhValue} />
         </div>
     );
 }
--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+interface share {
+    position: number;
+}
 
 function ShoveBoxButton({
     position,
@@ -13,8 +16,7 @@ function ShoveBoxButton({
     );
 }
 
-function MoveableBox(): JSX.Element {
-    const [position, setPosition] = useState<number>(10);
+function MoveableBox({ position }: share): JSX.Element {
     return (
         <div
             data-testid="moveable-box"
@@ -32,19 +34,18 @@ function MoveableBox(): JSX.Element {
 }
 
 export function ShoveBox(): JSX.Element {
-    const box = MoveableBox();
-
+    const [position, setPosition] = useState<number>(10);
     return (
         <div>
             <h3>Shove Box</h3>
-            {/* <span>The box is at: {box.position}</span>
+            <span>The box is at: {position}</span>
             <div>
                 <ShoveBoxButton
-                    position={box.position}
-                    setPosition={box.setPosition}
+                    position={position}
+                    setPosition={setPosition}
                 ></ShoveBoxButton>
-                {box}
-            </div> */}
+                <MoveableBox position={position} />
+            </div>
         </div>
     );
 }
